45e3ae93f4e9fe4699461ddd22921279f9e00259
isu-Hugo <ehugo209@gmail.com>
2025-03-30 17:25:30 -0300

    cmt

--- src/js/script.js
+++ src/js/script.js
@@ -1,20 +1,4 @@
-function load(){
-    let linhas = document.getElementsByTagName('tr');
-
-    celulas = linhas[1].getElementsByTagName('td');
-    for(i = 0; i<8; i++){
-        img = celulas[i].getElementsByTagName('img');
-        img[0].src = './src/images/bp.png';
-    }
-    
-    celulas = linhas[6].getElementsByTagName('td');
-    for(i = 0; i<8; i++){
-        img = celulas[i].getElementsByTagName('img');
-        img[0].src = './src/images/wp.png';
-    }
 
 
-}
-
-load();
+//document.getElementsByTagName('td')[1].classList
  
